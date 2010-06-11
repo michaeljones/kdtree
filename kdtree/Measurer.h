977bbf0b76d2e42d87107c2b821cf7f5d8554784
michaelj <michaelj@al.com.au>
2010-06-11 16:42:24 +1000

    Removed unused lengthSq method from measurer
    Based on an necessary concept in the code that is no longer required.

--- kdtree/Measurer.h
+++ kdtree/Measurer.h
@@ -10,19 +10,6 @@ public:
   Measurer() {}
 
   template< typename P, unsigned int DIM >
-  typename P::base_type lengthSq( const P& p ) const
-  {
-    typename P::base_type length( 0 );
-
-    for ( unsigned int i=0; i<DIM; ++i )
-    {
-      length += p[ i ] * p[ i ];
-    }
-
-    return length;
-  }
-
-  template< typename P, unsigned int DIM >
   typename P::base_type distanceSq( const P& a, const P& b ) const
   {
     typename P::base_type length( 0 );
